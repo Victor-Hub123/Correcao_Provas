40e64a977f97995a992e8ca72bf993d9c4b0079e
Victor Oliveira <victorcriri313@gmail.com>
2025-05-16 08:56:16 -0300

    6

---
+++ Prova_1/exercicio3.js
@@ -0,0 +1,15 @@
+let opcao = 2;
+
+switch (opcao) {
+    case 1:
+        console.log("Você escolheu cadastrar produto");
+        break;
+    case 2:
+        console.log("Você escolheu listar produtos");
+        break;
+    case 3:
+        console.log("Você escolheu sair");
+        break;
+    default:
+        console.log("Opção inválida");
+}
---
+++ Prova_1/exercicio4.js
@@ -0,0 +1,9 @@
+let numeros = [5, 2, 3, 2, 4];
+let soma = 0;
+
+for (i = 0; i < numeros.length; i++) {
+    //soma = soma + numeros[i];
+    soma += numeros[i];
+}
+
+console.log(soma);
